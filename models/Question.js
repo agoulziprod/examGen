a4405e29118607a7e32788890b2d58d3c5edb00a
agoulzi <agoulziprod@gmail.com>
2018-12-09 00:27:00 +0100

    model questions

--- models/Question.js
+++ models/Question.js
@@ -3,23 +3,28 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const Question = new Schema({
-  question: {
-    type: String,
-    required: true
-  },
-  hasOrder: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  test: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    question: {
+        type: String,
+        required: true
+    },
+    hasOrder: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    type: {
+        required: true,
+        type: string,
+        default: 'radio'
+    },
+    test: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 mongoose.model('question', Question);
