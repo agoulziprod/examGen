95e4b01b0e91fdaaebbf259d0eeb85a34cf4df66
agoulzi <mohammed.agoulzi50@gmail.com>
2019-01-06 00:50:37 +0100

    score calculated successfully

--- routes/reponses.js
+++ routes/reponses.js
@@ -51,42 +51,86 @@ router.post('/', ensureAuthenticated, (req, res) => {
   let reponses, reponsesObj = {};
 
   //now inserting data into our object and cleaning it after that
-  reponses = Object.assign({},req.body);
+  reponses = Object.assign({}, req.body);
 
   delete reponses.test;
   delete reponses.testInstance;
   // i got to convert the object reponses into an array
   // var varo = Object.entries(reponses);
-  let reponsesArray,newArray=[];
-  reponsesArray=Object.keys(reponses);
-  reponsesArray.forEach(key=>{
-    let temp={};
-    temp[key]=reponses[key];
+  let reponsesArray, newArray = [];
+  reponsesArray = Object.keys(reponses);
+  reponsesArray.forEach(key => {
+    let temp = {};
+    temp[key] = reponses[key];
     newArray.push(temp);
 
   })
+console.log('hna nchofo array jdid kidayr wach question: rep olla chi haja khra')
+console.log(newArray)
 
-  
   //then append it into reponsesObj
   //store it and redirect user
   reponsesObj = Object.assign(reponsesObj, {
-    test:req.body.test,
+    test: req.body.test,
     testInstance: req.body.testInstance,
     apprenant: req.user.id,
-    sentTime:Date.now(),
-    reponses:newArray
+    sentTime: Date.now(),
+    reponses: newArray
   })
   new Reponses(reponsesObj)
-  .save()
-  .then(rep => {
-    // hna khass n7asbo score o nsiftoh lih
-    
-    req.flash('success_msg', 'Le test a été ajouté avec succées');
-    res.redirect('/testInstances');
-  })
-  
-  
-  
+    .save()
+    .then(rep => {
+      // hna khass n7asbo score o nsiftoh lih
+      QuestionReponse.find({})
+        .then(lesReponses => {
+
+          lesReponses.forEach(el => {
+            delete el._doc.reponse;
+            // delete el._doc.question;
+            delete el._doc.__v;
+            delete el._doc.creator;
+            delete el._doc.date;
+          });
+          // daba 3andna gha les rep qui sont true
+          const trueQuestions = lesReponses
+            .filter(question => question.isTrue == true)
+            .map(question => question._id);
+
+            console.log('hna les type')
+  const stringTrueQ=trueQuestions.map(e=>e=String(e))
+  console.log('wach t applika skfkef')
+
+  stringTrueQ.forEach(e=>  console.log(typeof e))
+
+//daba comparaison mabine array dialna o array lli jbna mn base de donnée
+let onlyRep=newArray.map(el=>{
+  return Object.values(el).toString()})
+// now i got to compaire every item from this skafandria table to the trueQuestions table
+let score=0;
+onlyRep.forEach(el=>{
+console.log("index of"+el+'est')
+console.log(stringTrueQ.indexOf(el)>0? 1 : 0)
+  score+=stringTrueQ.indexOf(el)>0? 1 : 0;
+  console.log("score")
+  console.log(score)
+
+})
+// console.log("newArray")
+// console.log(newArray)
+// console.log("skafandria")
+// console.log(skafandria)
+// console.log(JSON.stringify(skafandria, null, 4));
+// console.log("trueQuestions")
+// console.log(trueQuestions)
+          req.flash('success_msg', 'Vos réponses ont été enregistré avec succès, vuos avez eu un score de : ');
+          res.redirect('/testInstances');
+        })
+
+
+    })
+
+
+
   // res.send(reponsesObj);
 });
 
